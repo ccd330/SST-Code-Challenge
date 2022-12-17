3f1801212c7072a7dbe2991e92858d6d552ae962
ccd330 <claudiacdavis@gmail.com>
2022-12-17 14:58:11 -0500

    removed extraneous files, kept testing file but need to add styling

--- timeline-challenge/src/App.js
+++ timeline-challenge/src/App.js
@@ -1,22 +1,37 @@
-import React, {useState} from 'react';
-import Timeline from './timeline';
-import EventForm from './form';
+//import React, {useState} from 'react';
+// import Timeline from './timeline';
+// import EventForm from './form';
+
+// const App = () => {
+//   const [events, setEvents] = useState([]);
+
+//   const addEvent = (event) => {
+//     setEvents([...events, event]);
+//   };
+
+//   return (
+//     <div className="app">
+//       <EventForm onSubmit={addEvent} />
+//       <Timeline events={events} />
+//     </div>
+//   );
+// };
+
+// export default App;
+import React from 'react';
+import Timeline from './Timeline';
 
 const App = () => {
-  const [events, setEvents] = useState([]);
-
-  const addEvent = (event) => {
-    setEvents([...events, event]);
-  };
-
   return (
-    <div className="app">
-      <EventForm onSubmit={addEvent} />
-      <Timeline events={events} />
+    <div>
+      <Timeline />
     </div>
   );
 };
 
 export default App;
+
+
+
 
 
--- timeline-challenge/src/timeline.js
+++ timeline-challenge/src/timeline.js
@@ -1,44 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Timeline = ({ events }) => {
-  // Sort the events by start time
-    events.sort((a, b) => a.start - b.start);
+const Timeline = () => {
+  const [events, setEvents] = useState([]); // initialize an empty array of events
 
-    return (
-        <div className="timeline">
+  // utility function to find the minimum number of required levels to stack the events
+    const findMinLevels = () => {
+        // sort the events by start time
+        const sortedEvents = events.sort((a, b) => a.start - b.start);
+
+        // initialize an array of levels, where each level represents the end time of the event in that level
+        const levels = [];
+
+        // iterate through the sorted events
+        for (const event of sortedEvents) {
+        // find the first level that is available for this event (i.e., level end time is before the start time of the event)
+        const availableLevel = levels.findIndex(levelEndTime => levelEndTime <= event.start);
+
+        // if no level is available, push a new level with the end time of the event
+        if (availableLevel === -1) {
+            levels.push(event.end);
+        } else {
+            // otherwise, update the end time of the available level with the end time of the event
+            levels[availableLevel] = event.end;
+        }
+        }
+
+        // return the number of levels used
+        return levels.length;
+    };
+
+    // event handler for the submit button of the form
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        // get the form values
+        const start = event.target.start.value;
+        const end = event.target.end.value;
+
+        // validate the form values
+        if (start < 0 || end > 86400 || start >= end) {
+        return; // do not add the event if the values are invalid/
+        }
+
+        // add the new event to the list of events
+        setEvents([...events, { start, end }]);
+    };
+
+return (
+    <div>
+        {/* render the timeline */}
+        <div style={{ display: 'flex' }}>
+        
+        {/* render the level numbers */}
+        <div style={{ width: '50px', position: 'absolute', top: 0 }}>
+            {Array.from(Array(findMinLevels())).map((_, index) => (
+                <div key={index} style={{ height: '20px' }}>
+                {index + 1}
+                </div>
+            ))}
+        </div>
         {events.map((event, index) => {
-            // Initialize an array to keep track of which levels are occupied at a given time
-            const occupiedLevels = new Array(events.length).fill(false);
+            // calculate the width and offset of the event based on its start and end times
+            const width = (event.end - event.start) / 86400 * 100;
+            const offset = event.start / 86400 * 100;
 
-            // Find the lowest unoccupied level for this event
-            let level = 0;
-            while (occupiedLevels[level]) {
-            level++;
-            }
+            // calculate the level of the event based on the number of minimum required levels
+            const minLevels = findMinLevels();
+            const level = index % minLevels;
 
-            // Mark this level as occupied
-            for (let i = event.start; i < event.end; i++) {
-            occupiedLevels[level] = true;
-            }
-
-            // Render the event block on the timeline
+            // return the event element
             return (
             <div
                 key={index}
-                className="event-block"
                 style={{
-                top: level * 50,
-                width: event.end - event.start,
-                left: event.start,
+                position: 'absolute',
+                left: `${offset}%`,
+                width: `${width}%`,
+                top: `${level * 20}px`,
+                backgroundColor: 'red',
+                height: '20px',
                 }}
-            >
-                {event.name}
-            </div>
+            />
             );
         })}
         </div>
-    );
-};
 
+        {/* render the form to create new timeline entries */}
+        <h2>Event Input</h2>
+        <form onSubmit={handleSubmit}>
+        <label>
+            Start:
+            <input type="number" name="start" />
+        </label>
+        <label>
+            End:
+            <input type="number" name="end" />
+        </label>
+        <button type="submit">Add Event</button>
+        </form>
+    </div>
+);
+
+}
 export default Timeline;
+
+
 
